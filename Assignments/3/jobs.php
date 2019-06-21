7b9fab10bd007bae81114b2180508935c3400791
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2019-06-21 16:11:31 +1000

    Create jobs.php

---
+++ Assignments/3/jobs.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+    <?php include 'header.inc'; ?>
+    <script src="scripts/apply.js"></script>
+    <title>Available Jobs</title>
+    <!-- Description: List of available jobs -->
+    <!-- Author: lewis 101533222 -->
+    <!-- Date: 19/05/19 -->
+    <!-- Validated: OK <19/05/19> -->
+</head>
+
+<body>
+    <div id="page-container">
+        <div id="content-wrap">
+            <?php include 'menu.inc'; ?>
+            <main class="container-fluid">
+                <h1>Available Jobs</h1>
+                <article class="col-sm-8">
+                    <section>
+                        <!-- Details for these jobs was found on seek-->
+
+                        <h2 id="job1" class="job_ad">Technical Writer</h2>
+                        <hr />
+                        <img src="images/company1.jpeg" alt="Company Image" />
+                        <br />
+                        <cite>
+                            https://cdn.crowdfundinsider.com/wp-content/uploads/2017/06/Fintech.jpg
+                        </cite>
+                        <h3>Details</h3>
+                        <ul>
+                            <li>Position Reference Number: <span id="job1_id">JOBTW</span> </li>
+                            <li>Location: Sydney, North Shore &#38; Northern Beaches</li>
+                            <li>Salary: $60,000 - $79,999</li>
+                            <li>Work Type: Full Time</li>
+                            <li>Classification: Information &#38; Communication
+                                Technology, Technical Writing</li>
+                        </ul>
+
+                        <h3>The Company</h3>
+                        <p>
+                            We are a Fintech firm that focuses on technology and
+                            financial
+                            services. As the Technical Writer/Document Control Officer
+                            you
+                            will have minimum 3 years of experience writing for Web
+                            Applications, write product specification documents
+                            (technical
+                            and non-technical) based upon the audience
+                        </p>
+                        <h3>The Role</h3>
+                        <p>
+                            Working with our project manager, development team and
+                            stakeholders, you will be responsible for preparing user
+                            tutorials, quick-start-guides, FAQ’s and product
+                            specification
+                            documents (technical and non-technical) based upon the
+                            audience.
+
+                        </p>
+                        <h3>Essential Skills</h3>
+                        <ol>
+                            <li>Experience in financial services would be highly
+                                regarded</li>
+                            <li>Multilingual would be highly regarded</li>
+                            <li>Able to write Chinese would be highly regarded</li>
+                            <li>Experience in marketing would be a bonus</li>
+                            <li>Experience or knowledge of Windows Server, VMWare, AD,
+                                ADFS,
+                                Azure, Exchange, Networking, Firewalls, Security
+                                Appliances</li>
+                        </ol>
+                        <h3>Prefered Elements</h3>
+                        <ul>
+                            <li>3 - 5 years’ experience in a Web Application, API and
+                                Mobile application technical writer</li>
+                            <li>Prepare user tutorials, quick-start-guides and FAQ’s</li>
+                            <li>Able to formulate documentation which can translate
+                                technical to
+                                non-technical</li>
+                            <li>High attention to detail, spelling and grammatical</li>
+                            <li>Maintain social media including Facebook page, twitter,
+                                blog,
+                                website contents</li>
+                            <li>Exceptional communication skills</li>
+                            <li>Storyboarding and storytelling</li>
+                        </ul>
+                        <h3>The Offer</h3>
+                        <ul>
+                            <li>Competitive remuneration package</li>
+                            <li>Brand New office located in the Heart of Chatswood CBD</li>
+                            <li>Newly created role with global exposure across the
+                                business</li>
+                        </ul>
+                        <cite>
+                            Reference:
+                            <a href="https://www.seek.com.au/job/38330181">seek</a>,
+                            Accessed 13 Mar 2019
+                        </cite>
+                    </section>
+                    <a class="button" id="job1_apply" href="apply.php">Apply Here</a>
+                    <section>
+
+                        <h2 id="job2" class="job_ad">Senior Full Stack Web Application Software Engineer</h2>
+                        <hr />
+                        <img src="images/company2.png" alt="Company Image" />
+                        <br />
+                        <cite>
+                            https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI9S5mvnAaNCIYfW0bQZOj_5pXniKjRdChPSi6CK-9ls8Oof5i
+                        </cite>
+                        <h3>Details</h3>
+                        <ul>
+                            <li>Position Referece Number: <span id="job2_id">JOBFS</span></li>
+                            <li>Salary: $60,000 - $79,999</li>
+                            <li>Location: Gold Coast</li>
+                            <li>Work Type: Full Time</li>
+                            <li>Classification: Information &#38; Communication
+                                Technology, Engineering - Software</li>
+                        </ul>
+
+                        <h3>About the business and the role</h3>
+                        <p>
+                            Formed in 1995, CyTrack is a 100% Australian owned company
+                            based on the Gold Coast.
+                            CyTrack is a Microsoft Gold Application Developer and Silver
+                            Cloud Competency Partner, awarded as winner Microsoft
+                            Partner
+                            Awards for 'Excellence in Industry and Platform Innovation.
+                            Job tasks and responsibilities
+                            <br />
+                            You will join our software engineering team and you will be
+                            specialising in modern web technologies enhancing a range of
+                            apps and services in our modular enterprise suite. Reporting
+                            to the Software Engineering Manager.
+                        </p>
+                        <h3>Essential Engineering skills required in following
+                            technologies:</h3>
+                        <ol>
+                            <li>C#, JavaScript, TypeScript
+                                Asp.Net MVC and Web API
+                                SQL Ado.Net
+                                AngularJS, Angular 5
+                                ReactJS, React-Redux</li>
+                            <li>Qualification in Software Design and Development</li>
+                            <li>Practical knowledge implementing Software Design
+                                Patterns</li>
+                            <li>Knowledge of Database management and optimisation</li>
+                            <li>Practical knowledge of development high quality UI/UX</li>
+                            <li>Strong knowledge of front end technologies:
+                                <ul>
+                                    <li>HTML5/CSS3/JavaScript ES6+</li>
+                                </ul>
+                            </li>
+                            <li>Knowledge and understanding of web protocols:
+                                <ul>
+                                    <li>HTTP/HTTPS/TLS/SSL/WEBSOCKET/REST</li>
+                                </ul>
+                            </li>
+                        </ol>
+                        <h3>Preferred abilities</h3>
+                        <ul>
+                            <li>Understanding of Agile processes</li>
+                            <li>Unit Testing</li>
+                            <li>Continuous Integration</li>
+                            <li>Continuous Delivery</li>
+                        </ul>
+                        <cite>
+                            Reference:
+                            <a href="https://www.seek.com.au/job/38537145?type=standout%26searchrequesttoken=f83d5530-611c-483a-84be-d7f39357691a">seek</a>
+                            Accessed 13 Mar 2019
+                        </cite>
+                    </section>
+                    <a class="button" id="job2_apply" href="apply.php">Apply Here</a>
+                </article>
+
+                <aside class="col-sm-4">
+                    <h2>List of available jobs</h2>
+                    <hr />
+                    <ul>
+                        <li><a href="jobs.phpjob1">Technical Writer</a></li>
+                        <li><a href="jobs.phpjob2">Senior Full Stack Web Application Software
+                                Engineer</a></li>
+                    </ul>
+                </aside>
+            </main>
+            <?php include 'footer.inc'; ?>
+        </div>
+    </div>
+</body>
+
+</html>
