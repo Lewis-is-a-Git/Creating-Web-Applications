3840a7b07b6e1abd594845102bc35d66af31022b
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2019-06-21 16:13:45 +1000

    Create settings.php

---
+++ Assignments/3/settings.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "feenix-mariadb.swin.edu.au";
+    $user = "s101533222";
+    $pwd = "sqlpassword";
+    $sql_db = "s101533222_db";
+    $db = @mysqli_connect($host,$user,$pwd,$sql_db);
+?>
